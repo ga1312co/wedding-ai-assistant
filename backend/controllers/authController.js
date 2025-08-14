d4e57cb9e8998ce43bd0a14e03336fc18a8b57bd
Gabriel <ga1312co-s@student.lu.se>
2025-08-14 07:38:06 +0200

    log missing env variables

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,7 @@
 const login = (req, res) => {
+  if (!process.env.LOGIN_PASSWORD) {
+    console.warn('LOGIN_PASSWORD not set; all logins will fail.');
+  }
   const { password } = req.body;
   if (password === process.env.LOGIN_PASSWORD) {
     res.json({ success: true });
